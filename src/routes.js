b4347a6adea16e426b088a377fde01a8000d2824
jonathan010603 <jonathan010603@gmail.com>
2022-09-06 12:57:18 -0300

    feat (GET all users): gET with pagination working
    - GET requests to /user will return all users in the database three by three

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersController = require('./usersController.js');
 
 router.post('/user', usersController.addUser);
+router.get('/user', usersController.getUsers);
 router.get('/:id', usersController.getUserById);
 
 module.exports = router;
--- src/usersController.js
+++ src/usersController.js
@@ -3,16 +3,31 @@ const mongoose = require('mongoose');
 
 exports.addUser = (req, res) => {
         let user = new Users(req.body);
-        user.save(err => {
-            err
-                ? res.status(500).send({message: `Failed to add user... ${err.message}`})
-                : res.status(201).send(user.toJSON())
-        })
+        user.save(err => err
+            ? res.status(500).send({ message: `Failed to add user... ${err.message}` })
+            : res.status(201).send(user.toJSON())
+        );
+}
+
+exports.getUsers = async (req, res) => {
+    const { page = 1, limit = 3 } = req.query;
+
+    try {
+        const users = await Users.find()
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec();
+
+        const count = await Users.countDocuments();
+        const totalPages = Math.ceil(count / limit);
+        res.json({ users, totalPages, currentPage: page });
+    }
+    catch (err) { res.status(500).send({ message: err.message }) }
 }
 
 exports.getUserById = async (req, res) => {
     try { const user = await Users.findById(req.params.id) } 
     catch (err) { return res.status(404).json({ message: "Id not found" }) }
-
+    
     return res.json({ message: "Success" });
 }
